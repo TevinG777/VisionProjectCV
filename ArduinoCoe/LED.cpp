fc4ca1a868bcc4d95fc48806a3946312bbec04b4
TevinG777 <gajadhartevin@gmail.com>
2023-08-29 23:46:37 -0400

    finished with lcd

--- ArduinoCoe/LED.cpp
+++ ArduinoCoe/LED.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
 // put function declarations here:
 int LED = 13;
 int LED2 = 12;
 int LED3 = 11;
+int seconds = 0;
+
+LiquidCrystal lcd_1(8, 7, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,13 +16,13 @@ void setup() {
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
 
+    //Set up the LCD Display
+    lcd_1.begin(16, 2);
+
 }
 
 void loop() {
   if(Serial.available() > 0){
-    unsigned long currentTime = millis();
-    const long interval = 250;
-    unsigned previousTime = 0;
 
     String msgP = Serial.readString();
 
@@ -29,17 +33,30 @@ void loop() {
     if(msgPf > 1000 && msgPf < 15000){
         digitalWrite(LED, HIGH);
         digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+        
+        //PRitn mid range to LCD
+        lcd_1.setCursor(0, 0);
+        lcd_1.print("Mid Range");
     }
-    else{
+    else if(msgPf <= 1000 || msgPf >= 15000){
         digitalWrite(LED, LOW);
         digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, LOW);
+        
+        //PRitn mid range to LCD
+        lcd_1.setCursor(0, 0);
+        lcd_1.print("FaR Range");
+    }
+    else{
+      digitalWrite(LED, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, HIGH);
     }
         
   }
 
   
-  else{
-    digitalWrite(LED3, HIGH);
-  }
+
 }
 
