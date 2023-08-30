3b106ec235015f3fd74d0f11a02b6094dbec7be7
TevinG777 <gajadhartevin@gmail.com>
2023-08-29 20:13:46 -0400

    value every 1 second but laggy vid

--- ArduinoCoe/LED.cpp
+++ ArduinoCoe/LED.cpp
@@ -3,13 +3,14 @@
 // put function declarations here:
 int LED = 13;
 int LED2 = 12;
-
+int LED3 = 11;
 
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(9600);
     pinMode(LED, OUTPUT);
     pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
 
 }
 
@@ -23,24 +24,22 @@ void loop() {
 
     //Convert serial input to a float
     float msgPf = msgP.toFloat();
-
-    //take sample of data every .25 seconds 
-    if(currentTime - previousTime >= interval){
-        previousTime = currentTime;
     
-        //if the float is between 1000-15000 then set the LED to HIGH if not green LED On
-        if(msgPf > 1000 && msgPf < 15000){
-            digitalWrite(LED, HIGH);
-            digitalWrite(LED2, LOW);
-        }
-        else{
-            digitalWrite(LED, LOW);
-            digitalWrite(LED2, HIGH);
-        }
-        
+    //if the float is between 1000-15000 then set the LED to HIGH if not green LED On
+    if(msgPf > 1000 && msgPf < 15000){
+        digitalWrite(LED, HIGH);
+        digitalWrite(LED2, LOW);
     }
-
+    else{
+        digitalWrite(LED, LOW);
+        digitalWrite(LED2, HIGH);
+    }
+        
   }
 
+  
+  else{
+    digitalWrite(LED3, HIGH);
+  }
 }
 
