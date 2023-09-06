6bb94f0819d61d68ab83a610f83137bca1485473
TevinG777 <gajadhartevin@gmail.com>
2023-09-06 13:42:13 -0400

    new color tuner

--- ColorTuning.py
+++ ColorTuning.py
@@ -1,15 +1,10 @@
 import cv2
 import numpy as np
 
-# Create a callback function for mouse events
-def get_color(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        # Get the color at the clicked point in HSV
-        hsv_color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        color = hsv_color[y, x]
-        print("HSV Color:", color)
+def nothing(x):
+    pass
 
-# Create a VideoCapture object
+# Create a VideoCapture object for the camera
 cap = cv2.VideoCapture(0)
 
 # Error handling
@@ -17,33 +12,73 @@ if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
-# Create a window to display the camera feed
-cv2.namedWindow("Camera Feed")
+# Create a window
+cv2.namedWindow('image')
 
-# Set up a mouse callback function to get color values
-cv2.setMouseCallback("Camera Feed", get_color)
+# Create trackbars for color change
+# Hue is from 0-179 for OpenCV
+cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+# Set default value for Max HSV trackbars
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize HSV min/max values
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
 while True:
     # Capture a frame from the camera
     ret, frame = cap.read()
 
-    # Can not read frame
+    # Error handling
     if not ret:
         print("Error: Could not read frame.")
         break
 
-    # Apply erosion and dilation to the frame
+    # Get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin', 'image')
+    sMin = cv2.getTrackbarPos('SMin', 'image')
+    vMin = cv2.getTrackbarPos('VMin', 'image')
+    hMax = cv2.getTrackbarPos('HMax', 'image')
+    sMax = cv2.getTrackbarPos('SMax', 'image')
+    vMax = cv2.getTrackbarPos('VMax', 'image')
+
+    # Set minimum and maximum HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
     kernel = np.ones((5, 5), np.uint8)
-    erosion = cv2.erode(frame, kernel, iterations=1)
-    erosion_dilation = cv2.dilate(erosion, kernel, iterations=1)
 
-    # Display the camera feed and the processed frames
-    cv2.imshow("Camera Feed", erosion_dilation)
+    # Convert to HSV format and apply color threshold
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    mask_eroded = cv2 . erode ( mask , kernel , iterations = 3)
+    mask_eroded_dilated = cv2 . dilate ( mask_eroded , None , iterations = 10)
 
-    # Exit program when q is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Print if there is a change in HSV value
+    if((phMin != hMin) or (psMin != sMin) or (pvMin != vMin) or (phMax != hMax) or (psMax != sMax) or (pvMax != vMax)):
+        print("(hMin = %d, sMin = %d, vMin = %d), (hMax = %d, sMax = %d, vMax = %d)" % (hMin, sMin, vMin, hMax, sMax, vMax))
+        phMin = hMin
+        psMin = sMin
+        pvMin = vMin
+        phMax = hMax
+        psMax = sMax
+        pvMax = vMax
+
+    # Display result image
+    cv2.imshow('image', mask_eroded_dilated)
+
+    # Exit program when 'q' is pressed
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
-# Release the camera and close the windows
+# Release the camera and close the window
 cap.release()
 cv2.destroyAllWindows()
