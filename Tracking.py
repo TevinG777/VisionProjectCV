4e52998f8eee4e3b3dcdf278780ee35d3b6a0753
TevinG777 <gajadhartevin@gmail.com>
2023-09-27 12:07:51 -0400

    Websocket Data

--- Tracking.py
+++ Tracking.py
@@ -1,109 +1,99 @@
 import cv2
 import numpy as np
-import serial.tools.list_ports
-import time
 import asyncio
 import websockets
 import json
+import time
+
+# Define WebSocket server details
+websocket_server_address = "ws://10.20.1.93:8765"  # Replace with your WebSocket server address
+
+async def send_data_to_websocket(data):
+    async with websockets.connect(websocket_server_address) as websocket:
+        await websocket.send(json.dumps(data))
+
+async def process_camera_data():
+    last_send = 0  # Initialize the last_send variable
+    # Create a VideoCapture object
+    url = "10.20.1.40:81/stream"
+    cap = cv2.VideoCapture(url)
+
+    # Error handling
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Error: Could not read frame.")
+            break
+
+        squares = isSquare(frame)
+
+        if len(squares) == 0:
+            current_time = time.time() - last_send
+            if current_time >= 1:
+                print("0,0")
+                await send_data_to_websocket({"x": 0, "area": 0})
+                last_send = time.time()
+
+        cv2.drawContours(frame, squares, -1, (225, 0, 0), 3)
+        cv2.imshow("Frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 def isSquare(frame):
-    #initilize variable to store the last time a record was sent
     global last_send
-    
-    #convert frame from camera to HSV color space
+
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    #define threshold for edge detection for multimeter
     lower_color = np.array([0, 74, 55])
     upper_color = np.array([11, 255, 255])
-    #Hue: We dont care what value because we are looking for white
-    #Saturation: How color color
-    #Value: How bright the color is
-    
-    #define kernel for erosion
+
     kernel = np.ones((5, 5), np.uint8)
 
-    #define mask for color
     mask = cv2.inRange(frame, lower_color, upper_color)
-    mask_eroded = cv2 . erode ( mask , kernel , iterations = 2)
-    mask_eroded_dilated = cv2 . dilate ( mask_eroded , None , iterations = 8)
+    mask_eroded = cv2.erode(mask, kernel, iterations=2)
+    mask_eroded_dilated = cv2.dilate(mask_eroded, None, iterations=8)
 
-    #create list of contour points 
     contours, _ = cv2.findContours(mask_eroded_dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    #filter points to only include squares
+
     squares = []
     for cnt in contours:
-        #define epsilon for approximation, determines the accuracy of the approximation, and if it is closed
-        epsilon = 0.1*cv2.arcLength(cnt, True)
-        
+        epsilon = 0.1 * cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, epsilon, True)
         minArea = 100
-        
-        #if the contour is a square, and the area is greater than the minimum area, add it to the list
+
         if len(approx) == 4 and cv2.contourArea(cnt) > minArea:
             squares.append(cnt)
             area = cv2.contourArea(cnt)
-            
-            #define a rectangle and get x value
             rect = cv2.boundingRect(cnt)
             x, y, w, h = rect
-                  
-            #calclate the time elaspsed since last record was sent
+
             current_time = time.time() - last_send
-            
+
             if current_time >= 1:
-                #print send message to Arudino
                 print(str(x) + "," + str(area))
-                #ser.write((str(x) + "," + str(area)).encode('utf-8'))
-                
-                #update last send time
+                asyncio.run(send_data_to_websocket({x, area}))
                 last_send = time.time()
-        
-                  
-            
-      
+
     return squares
 
-# Create a VideoCapture object
-url = "10.20.1.40:81/stream"
-cap = cv2.VideoCapture(url)
+if __name__ == "__main__":
+    # Initialize the asyncio event loop
+    loop = asyncio.get_event_loop()
 
-# Error handling
-if not cap.isOpened():
-    print("Error: Could not open camera.")
-    exit()
+    # Start the camera data processing as a task
+    camera_data_task = loop.create_task(process_camera_data())
 
-
-while True:
-    # Capture a frame from the camera
-    ret, frame = cap.read()
-
-    # Can not read frame
-    if not ret:
-        print("Error: Could not read frame.")
-        break
-
-    #display from with squares
-    squares = isSquare(frame)
-    
-    #check if squares array is empty, and if it is send 0,0 to Arduino over serial
-    if len(squares) == 0:
-        current_time = time.time() - last_send
-        if current_time >= 1:
-            print("0,0")
-            #ser.write(("0,0").encode('utf-8'))
-            last_send = time.time()
-    
-    cv2.drawContours(frame, squares, -1, (225, 0, 0), 3)
-    cv2.imshow("Frame", frame)
-    
-
-    # Exit program when q is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the camera and close the window
-cap.release()
-cv2.destroyAllWindows()
-
+    # Run the event loop
+    try:
+        loop.run_until_complete(camera_data_task)
+    except KeyboardInterrupt:
+        pass
