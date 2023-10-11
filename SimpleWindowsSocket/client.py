7eafd1af719d671510244d10c77e94c65914b813
TevinG777 <gajadhartevin@gmail.com>
2023-10-11 10:52:13 -0400

    changes

--- SimpleWindowsSocket/client.py
+++ SimpleWindowsSocket/client.py
@@ -25,8 +25,10 @@ async def client():
         # Connect to the WebSocket server
         async with websockets.connect("ws://10.20.1.93:8765") as websocket:
             while True:
+                # Receive data from the WebSocket server
                 message = await websocket.recv()
                 try:
+                    # Parse the JSON data
                     data = json.loads(message)
                     data1 = data.get("data1")
                     data2 = data.get("data2")
@@ -37,6 +39,7 @@ async def client():
                         ser.write(f"{data1},{data2}\n".encode('utf-8'))
                     else:
                         print("Invalid data received from WebSocket.")
+                    #error handling
                 except json.JSONDecodeError as e:
                     print(f"Error decoding JSON: {e}")
                 except Exception as e:
@@ -48,7 +51,9 @@ async def client():
     except serial.SerialException as e:
         print(f"Serial port error: {e}")
     finally:
+        # Close the serial port
         ser.close()
 
+# Run the client
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(client())
