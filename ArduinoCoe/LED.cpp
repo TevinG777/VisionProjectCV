f8bddfcca07c870a96d46ff17d3b32b5c4f30f81
TevinG777 <gajadhartevin@gmail.com>
2023-11-08 11:51:45 -0500

    vision motor code

--- ArduinoCoe/LED.cpp
+++ ArduinoCoe/LED.cpp
@@ -1,30 +1,47 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 
-// put function declarations here:
-int seconds = 0;
+const byte rxPin = 3; // Arduino RX = D3
+const byte txPin = 2; // Arduino TX = D2
+const int pwmPin1 = 6;
+const int pwmPin2 = 11;
+
 int in1 = 4; // variable names for the L298N
 int in2 = 5; // variable names for the L298N
 int in3 = 9; // variable names for the L298N
 int in4 = 10; // variable names for the L298N
 
-LiquidCrystal lcd_1(8, 7, 5, 4, 3, 2);
+int in2_1 = 7; // variable names for the L298N
+int in2_2 = 8; // variable names for the L298N
+int in2_3 = 12; // variable names for the L298N
+int in2_4 = 13; // variable names for the L298N
+
+SoftwareSerial mySerial(rxPin, txPin); // Begin a software serial on pins D3 and D2 to talk to the Pi
 
 void setup() {
-  // put your setup code here, to run once:
-    Serial.begin(9600);
-    pinMode (in1, OUTPUT); // all L298N digital pins are outputs
-    pinMode (in2, OUTPUT); // all L298N digital pins are outputs
-    pinMode (in3, OUTPUT); // all L298N digital pins are outputs
-    pinMode (in4, OUTPUT); // all L298N digital pins are outputs
+  Serial.begin(115200); // Begin the hardware D0 and D1 - we can use this Serial for debugging
+  mySerial.begin(9600); // Software serial for communicating with Pi
 
-    //Set up the LCD Display
-    lcd_1.begin(16, 2);
+  // Motor 1
+  pinMode(in1, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in3, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in4, OUTPUT); // all L298N digital pins are outputs
+
+  // Motor 2
+  pinMode(in2_1, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2_2, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2_3, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2_4, OUTPUT); // all L298N digital pins are outputs
+
+  // Configure PWM pins
+  pinMode(pwmPin1, OUTPUT);
+  pinMode(pwmPin2, OUTPUT);
 
 }
 
 void loop() {
-  if(Serial.available() > 0){
+  if(mySerial.available()){
 
     String msgP = Serial.readString();
 
@@ -47,86 +64,85 @@ void loop() {
 
 
     }
-    // Initilize strign vars
-    String posTxt = "Unkown";
 
-    //If the object is between the pos values 0, 100 set a posTxt variable to left, if it is from 100-350 set it to center, if it is 350-600 set it to right
-    if(pos > 0 && pos < 100){
-      posTxt = "Left";
-    }
-    else if(pos >= 100 && pos < 350){
-      posTxt = "Center";
-    }
-    else{
-      posTxt = "Right";
-    }
-    
     //if the float is between 1000-15000 then set the LED to HIGH if not green LED On
-    if(area > 1000 && area < 9000){
-        //Clear the LCD
-        lcd_1.clear();
+    if(area > 6000 && area < 9000){
 
         //Stop motor
         digitalWrite(in1, LOW);
         digitalWrite(in2, LOW);
         digitalWrite(in3, LOW);
         digitalWrite(in4, LOW);
+
         
-        //PRitn mid range to LCD
-        lcd_1.setCursor(0, 0);
-        lcd_1.print("Middle: " + posTxt);
+        digitalWrite(in2_1, LOW);
+        digitalWrite(in2_2, LOW);
+        digitalWrite(in2_3, LOW);
+        digitalWrite(in2_4, LOW);
 
-        //Print the area to the LCD
-        lcd_1.setCursor(0, 1);
-        lcd_1.print("Area: " + String(area));
-
+        // Set PWM values to stop the motors (0 for full stop)
+        analogWrite(pwmPin1, 0);
+        analogWrite(pwmPin2, 0);
+        
     }
     else if(area == 0.0){
-
-      //Clear the LCD
-      lcd_1.clear();
-
-      //Print STOP to LCD
-      lcd_1.setCursor(0, 0);
-      lcd_1.print("STOP");
-
-    }
-    else if(area <= 1000){
-
-        //Set the motor to go forward
+         //Stop motor
         digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
+        digitalWrite(in2, LOW);
         digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
+        digitalWrite(in4, LOW);
 
-        //Clear the LCD
-        lcd_1.clear();
+        //Stop motor
+        digitalWrite(in2_1, LOW);
+        digitalWrite(in2_2, LOW);
+        digitalWrite(in2_3, LOW);
+        digitalWrite(in2_4, LOW);
 
-        //Print mid range to LCD
-        lcd_1.setCursor(0, 0);
-        lcd_1.print("Far: " + posTxt);
 
-        //Print the area to the LCD
-        lcd_1.setCursor(0, 1);
-        lcd_1.print("Area: " + String(area));
+        // Set PWM values to stop the motors (0 for full stop)
+        analogWrite(pwmPin1, 0);
+        analogWrite(pwmPin2, 0);
+
     }
-    else{
-      //Clear the LCD
-      lcd_1.clear();   
+    else if(area <= 6000){
 
-      //Set motor to go backwards
+      // Set each group of motors to go forward
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH);
 
-      //PRitn mid range to LCD
-      lcd_1.setCursor(0, 0);
-      lcd_1.print("Close: " + posTxt);
+      digitalWrite(in2_1, HIGH);
+      digitalWrite(in2_2, LOW);
+      digitalWrite(in2_3, LOW);
+      digitalWrite(in2_4, HIGH);
 
-      //Print the area to the LCD
-      lcd_1.setCursor(0, 1);
-      lcd_1.print("Area: " + String(area));
+      // Set PWM values to control speed (0-255, adjust as needed)
+      analogWrite(pwmPin1, 255); // Full speed
+      analogWrite(pwmPin2, 255); // Full speed
+
+
+    }
+    else if(area >= 9000){
+      //Robot is to close move back
+
+      // Set each group of motors to go backward
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+
+
+      digitalWrite(in2_1, LOW);
+      digitalWrite(in2_2, HIGH);
+      digitalWrite(in2_3, HIGH);
+      digitalWrite(in2_4, LOW);
+
+      // Set PWM values to control speed (0-255, adjust as needed)
+      analogWrite(pwmPin1, 255); // Full speed
+      analogWrite(pwmPin2, 255); // Full speed
+
+
 
     }
         
--- ArduinoCoe/LEDserailRead.cpp
+++ ArduinoCoe/LEDserailRead.cpp
@@ -2,17 +2,18 @@
 
 const byte rxPin = 3; // Arduino RX = D3
 const byte txPin = 2; // Arduino TX = D2
+const int pwmPin1 = 6;
+const int pwmPin2 = 11;
 
 int in1 = 4; // variable names for the L298N
 int in2 = 5; // variable names for the L298N
 int in3 = 9; // variable names for the L298N
 int in4 = 10; // variable names for the L298N
 
-int in2_1 = 6; // variable names for the L298N
-int in2_2 = 7; // variable names for the L298N
-int in2_3 = 11; // variable names for the L298N
-int in2_4 = 12; // variable names for the L298N
-
+int in2_1 = 7; // variable names for the L298N
+int in2_2 = 8; // variable names for the L298N
+int in2_3 = 12; // variable names for the L298N
+int in2_4 = 13; // variable names for the L298N
 
 SoftwareSerial mySerial(rxPin, txPin); // Begin a software serial on pins D3 and D2 to talk to the Pi
 
@@ -21,17 +22,20 @@ void setup() {
   mySerial.begin(9600); // Software serial for communicating with Pi
 
   // Motor 1
-  pinMode (in1, OUTPUT); // all L298N digital pins are outputs
-  pinMode (in2, OUTPUT); // all L298N digital pins are outputs
-  pinMode (in3, OUTPUT); // all L298N digital pins are outputs
-  pinMode (in4, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in1, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in3, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in4, OUTPUT); // all L298N digital pins are outputs
 
-  //Motor 2
-  pinMode (in2_1, OUTPUT); // all L298N digital pins are outputs
-  pinMode (in2_2, OUTPUT); // all L298N digital pins are outputs
-  pinMode (in2_3, OUTPUT); // all L298N digital pins are outputs
-  pinMode (in2_4, OUTPUT); // all L298N digital pins are outputs
+  // Motor 2
+  pinMode(in2_1, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2_2, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2_3, OUTPUT); // all L298N digital pins are outputs
+  pinMode(in2_4, OUTPUT); // all L298N digital pins are outputs
 
+  // Configure PWM pins
+  pinMode(pwmPin1, OUTPUT);
+  pinMode(pwmPin2, OUTPUT);
 }
 
 void loop() {
@@ -40,7 +44,7 @@ void loop() {
     if (receivedChar == '1') {
       Serial.println("Received a 1");
 
-      //Set each group of motors to go forward
+      // Set each group of motors to go forward
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
       digitalWrite(in3, LOW);
@@ -51,11 +55,14 @@ void loop() {
       digitalWrite(in2_3, LOW);
       digitalWrite(in2_4, HIGH);
 
+      // Set PWM values to control speed (0-255, adjust as needed)
+      analogWrite(pwmPin1, 255); // Full speed
+      analogWrite(pwmPin2, 255); // Full speed
 
     } else if (receivedChar == '0') {
       Serial.println("Received a 0");
 
-      //Stop each group of motors
+      // Stop each group of motors
       digitalWrite(in1, LOW);
       digitalWrite(in2, LOW);
       digitalWrite(in3, LOW);
@@ -66,37 +73,9 @@ void loop() {
       digitalWrite(in2_3, LOW);
       digitalWrite(in2_4, LOW);
 
-
+      // Set PWM values to stop the motors (0 for full stop)
+      analogWrite(pwmPin1, 0);
+      analogWrite(pwmPin2, 0);
     }
-    // else if received 2 go right
-    else if (receivedChar == '2') {
-      Serial.println("Received a 2");
-
-      //Set each group of motors to go forward
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
-
-      digitalWrite(in2_1, LOW);
-      digitalWrite(in2_2, HIGH);
-      digitalWrite(in2_3, HIGH);
-      digitalWrite(in2_4, LOW);
-  }
-
-  // else if received 3 go left
-  else if (receivedChar == '3') {
-    Serial.println("Received a 3");
-
-    //Set each group of motors to go forward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-
-    digitalWrite(in2_1, HIGH);
-    digitalWrite(in2_2, LOW);
-    digitalWrite(in2_3, LOW);
-    digitalWrite(in2_4, HIGH);
   }
 }
