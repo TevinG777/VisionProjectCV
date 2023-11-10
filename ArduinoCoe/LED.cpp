ca999ddafd0f2146995926739e0337fb3e735f9c
TevinG777 <gajadhartevin@gmail.com>
2023-11-09 22:48:39 -0500

    finally works

--- ArduinoCoe/LED.cpp
+++ ArduinoCoe/LED.cpp
@@ -6,6 +6,9 @@ const byte txPin = 2; // Arduino TX = D2
 const int pwmPin1 = 6;
 const int pwmPin2 = 11;
 
+float area = 0.0;
+int pos = 0;
+
 int in1 = 4; // variable names for the L298N
 int in2 = 5; // variable names for the L298N
 int in3 = 9; // variable names for the L298N
@@ -43,82 +46,70 @@ void setup() {
 }
 
 void loop() {
-  if (mySerial.available()) {
-    static char buffer[64]; // Buffer Characters for incoming messages
-    static byte bufferIndex = 0;
+  static char buffer[64]; // Buffer Characters for incoming messages
+  static byte bufferIndex = 0;
   
+
+  if (mySerial.available()) {
     char receivedChar = mySerial.read();
 
-    if (receivedChar == '\n') { // Assuming '\n' is your message delimiter
+    if (receivedChar == '\n') { // Using new line character to split messages
       buffer[bufferIndex] = '\0'; // Null-terminate the buffer
       bufferIndex = 0;
-    
-      // Initialize variables to hold the float value for area and x pos
-      float area = 0.0;
-      int pos = 0;
-    
+
       // Parse the buffer
-      char *posStr = strtok(buffer, ","); //Parse the first value (pos)
-      char *areaStr = strtok(NULL, ","); //Parse the second value (area)
-    
+      char *posStr = strtok(buffer, ","); // Parse the first value (pos)
+      char *areaStr = strtok(NULL, ","); // Parse the second value (area)
+
       if (posStr != NULL && areaStr != NULL) {
         pos = atoi(posStr); // Convert the string to an int
         area = atof(areaStr); // Convert the string to a float
-      
       }
-  }  
-  // If the buffer is full, reset it
-  else {
-    buffer[bufferIndex] = receivedChar;
-    bufferIndex++;
-    
-    // Handle the case where the buffer is too small for your messages
-    if (bufferIndex >= sizeof(buffer) - 1) {
-      bufferIndex = 0; // Reset the buffer
+    } else {
+      // If the buffer is full, reset it
+      buffer[bufferIndex] = receivedChar;
+      bufferIndex++;
+
+      // Handle the case where the buffer is too small for your messages
+      if (bufferIndex >= sizeof(buffer) - 1) {
+        bufferIndex = 0; // Reset the buffer
+      }
     }
   }
-}
 
+  Serial.println(pos); // Print the area to the serial monitor (for debugging
+  Serial.println(area); // Print the position to the serial monitor (for debugging)
 
-    if(area == 0.0){
-         //Stop motor
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
+  if (area == 0.0) {
+    // Stop motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
 
-        //Stop motor
-        digitalWrite(in2_1, LOW);
-        digitalWrite(in2_2, LOW);
-        digitalWrite(in2_3, LOW);
-        digitalWrite(in2_4, LOW);
+    digitalWrite(in2_1, LOW);
+    digitalWrite(in2_2, LOW);
+    digitalWrite(in2_3, LOW);
+    digitalWrite(in2_4, LOW);
 
+    // Set PWM values to stop the motors (0 for full stop)
+    analogWrite(pwmPin1, 0);
+    analogWrite(pwmPin2, 0);
+  } else {
+    // Drive forward
+    // Set each group of motors to go forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
 
-        // Set PWM values to stop the motors (0 for full stop)
-        analogWrite(pwmPin1, 0);
-        analogWrite(pwmPin2, 0);
+    digitalWrite(in2_1, HIGH);
+    digitalWrite(in2_2, LOW);
+    digitalWrite(in2_3, LOW);
+    digitalWrite(in2_4, HIGH);
 
-    }
-    else{
-      //drive forward
-      // Set each group of motors to go forward
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
-
-      digitalWrite(in2_1, HIGH);
-      digitalWrite(in2_2, LOW);
-      digitalWrite(in2_3, LOW);
-      digitalWrite(in2_4, HIGH);
-
-      // Set PWM values to control speed (0-255, adjust as needed)
-      analogWrite(pwmPin1, 255); // Full speed
-      analogWrite(pwmPin2, 255); // Full speed
-
-    }
-        
+    // Set PWM values to control speed (0-255, adjust as needed)
+    analogWrite(pwmPin1, 255); // Full speed
+    analogWrite(pwmPin2, 255); // Full speed
   }
-
 }
-
